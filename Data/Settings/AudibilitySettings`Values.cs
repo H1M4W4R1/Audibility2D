592c2645fb0503f0326671b50d17438d34d5f7b9
Patryk Pastuszka <nov11157@gmail.com>
2025-08-19 00:10:41 +0200

    Added [SerializeField] to AudibilitySettings`Values.cs

--- Data/Settings/AudibilitySettings`Values.cs
+++ Data/Settings/AudibilitySettings`Values.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Systems.Audibility2D.Data.Settings
@@ -5,11 +6,12 @@ namespace Systems.Audibility2D.Data.Settings
     /// <summary>
     ///     Global audibility settings for all known audibility solutions
     /// </summary>
+    [Serializable]
     public sealed partial class AudibilitySettings
     {
-        public Color gizmosColorMinMuffling = Color.red;
-        public Color gizmosColorMaxMuffling = Color.green;
-        public Color gizmosColorMinAudibility = Color.red;
-        public Color gizmosColorMaxAudibility = Color.green;
+        [SerializeField] public Color gizmosColorMinMuffling = Color.red;
+        [SerializeField] public Color gizmosColorMaxMuffling = Color.green;
+        [SerializeField] public Color gizmosColorMinAudibility = Color.red;
+        [SerializeField] public Color gizmosColorMaxAudibility = Color.green;
     }
 }
