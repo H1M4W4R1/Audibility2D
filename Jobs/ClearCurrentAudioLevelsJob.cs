c679d054f94b7d610ad455096d2ce59424b8c66c
Patryk Pastuszka <nov11157@gmail.com>
2025-08-20 15:40:08 +0200

    Added [BurstCompile] to cleanup job.

--- Jobs/ClearCurrentAudioLevelsJob.cs
+++ Jobs/ClearCurrentAudioLevelsJob.cs
@@ -1,14 +1,17 @@
 ﻿using Systems.Audibility2D.Data.Native;
 using Systems.Audibility2D.Utility;
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
 
 namespace Systems.Audibility2D.Jobs
 {
+    [BurstCompile]
     public struct ClearCurrentAudioLevelsJob : IJobParallelFor
     {
         public NativeArray<AudioTileInfo> tileComputeData;
         
+        [BurstCompile]
         public void Execute(int index)
         {
             AudioTileInfo tileInfo = tileComputeData[index];
